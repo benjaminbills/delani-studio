1254e9252862fd4140dd8344251da38677deb6a1
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-03-01 00:04:26 +0300

    improve indentation for portfolio section function

--- js/app.js
+++ js/app.js
@@ -18,10 +18,14 @@ $(document).ready(function () {
   $.getJSON("imageData.json", function (data) {
     $.each(data, function (i, image) {
       $("div#portfolio").append(
-        `<div id=${image.id}  class="col-3 portfolio-image"> <img class="img-fluid" src="assets/portfolio/${image.imageName}" alt="" /> <div class="overlay">
-        <p class="portfolio-text" 
-        id=${image.idText} >${image.imageText}</p>
-      </div> </div>`
+        `<div id=${image.id}  class="col-3 portfolio-image"> 
+          <img class="img-fluid" src="assets/portfolio/${image.imageName}" alt="" /> 
+          <div class="overlay">
+            <p class="portfolio-text" id=${image.idText} >
+              ${image.imageText}
+            </p>
+          </div> 
+        </div>`
       );
       $(`#${image.id}`)
         .mouseenter(function () {
