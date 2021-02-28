fa1db1611efbfefc4550bbb10e14dc6a0ff0e4ac
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-02-28 23:42:11 +0300

    portfolio section complete

--- js/app.js
+++ js/app.js
@@ -15,4 +15,32 @@ $(document).ready(function () {
       $(showContents[i]).hide();
     });
   }
+  $.getJSON("imageData.json", function (data) {
+    $.each(data, function (i, image) {
+      $("div#portfolio").append(
+        `<div id=${image.id}  class="col-3 portfolio-image"> <img class="img-fluid" src="assets/portfolio/${image.imageName}" alt="" /> <div class="overlay">
+        <p style=" 
+        display: none;color: white;
+        font-size: 20px;
+        position: absolute;
+        top: 30%;
+        left: 50%;
+        -webkit-transform: translate(-50%, -50%);
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+        text-align: center;
+        text-shadow: 0 2px black;
+        " 
+        id=${image.idText} >${image.imageText}</p>
+      </div> </div>`
+      );
+      $(`#${image.id}`)
+        .mouseenter(function () {
+          $(`#${image.idText}`).show();
+        })
+        .mouseleave(function () {
+          $(`#${image.idText}`).hide();
+        });
+    });
+  });
 });
