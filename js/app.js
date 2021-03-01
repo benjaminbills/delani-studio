a9cb94bfc54664b40f8a109847237847810d9ce9
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-03-01 22:35:47 +0300

    add easy trasition to show and hide function

--- js/app.js
+++ js/app.js
@@ -5,34 +5,34 @@ $(document).ready(function () {
 
   for (let i = 0; i < hideContents.length; i++) {
     $(hideContents[i]).click(function () {
-      $(hideContents[i]).hide();
-      $(showContents[i]).show();
+      $(hideContents[i]).hide(300);
+      $(showContents[i]).show(300);
     });
   }
   for (let i = 0; i < showContents.length; i++) {
     $(showContents[i]).click(function () {
-      $(hideContents[i]).show();
-      $(showContents[i]).hide();
+      $(hideContents[i]).show(300);
+      $(showContents[i]).hide(300);
     });
   }
   $.getJSON("imageData.json", function (data) {
     $.each(data, function (i, image) {
       $("div#portfolio").append(
-        `<div id=${image.id}  class="col-4 col-sm-3 portfolio-image"> 
-          <img class="img-fluid" src="assets/portfolio/${image.imageName}" alt="" /> 
+        `<div id=${image.id}  class="col-4 col-sm-3 portfolio-image">
+          <img class="img-fluid" src="assets/portfolio/${image.imageName}" alt="" />
           <div class="overlay">
             <p class="portfolio-text" id=${image.idText} >
               ${image.imageText}
             </p>
-          </div> 
+          </div>
         </div>`
       );
       $(`#${image.id}`)
         .mouseenter(function () {
-          $(`#${image.idText}`).show();
+          $(`#${image.idText}`).show(300);
         })
         .mouseleave(function () {
-          $(`#${image.idText}`).hide();
+          $(`#${image.idText}`).hide(300);
         });
     });
   });
